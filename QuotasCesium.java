ca6a4151b09c1cb5fa1850b0e58b05350008ff7d
jcm300 <jcm300@live.com.pt>
2017-09-21 17:50:13 +0100

    Class Aluno almost finish, begining of interface of the app, and start of the Bussiness layer

---
+++ QuotasCesium.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quotascesium;
+
+/**
+ *
+ * @author José Martins
+ */
+public class QuotasCesium {
+
+    public Map<Integer,Aluno> getAlunos(){
+        
+    }
+    
+    public void pagarQuota(Integer numero, Double valor){
+        
+    }
+    
+    public Aluno getAluno(int num){
+        
+    }
+    
+    public void addAluno(Aluno a){
+        
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # QuotasCesium
+
+Micro-work for Software Systems of Development at Minho University.
+The objectives of the work is to be familiar with Swing and recap Java language.
+
+In a few words the work is to build a tiny app that show members of one Association.
+
+Features:
+- Add new members;
+- Pay quotas;
+- Change data of members.
